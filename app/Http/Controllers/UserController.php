2e6ce878719d949d17aad902a7a1130d7bc009d1
Akbar Prayuda Putra <akbarprayuda210806@gmail.com>
2025-10-16 20:19:38 +0700

    Users Create, Delete by ID, Update by ID, Get by ID

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Exceptions\UsersException;
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Arr;
 
 class UserController extends Controller
 {
@@ -31,7 +30,20 @@ class UserController extends Controller
    */
   public function store(Request $request)
   {
-    //
+    $validated = $request->validate([
+      'name' => 'required|string|max:255',
+      'email' => 'required|string|email|max:255|unique:users,email',
+      'password' => 'required|string|min:8|confirmed',
+      'nim' => 'required|unique:users,nim|string|max:255',
+      'role' => 'string|max:255',
+    ]);
+
+    $user = User::create($validated);
+
+    return response()->json([
+      'message' => 'User created successfully',
+      'user' => $user,
+    ], 201);
   }
 
   /**
@@ -39,7 +51,16 @@ class UserController extends Controller
    */
   public function show(string $id)
   {
-    //
+    $user = User::find($id);
+
+    if (!$user) {
+      throw new UsersException("User with id {$id} not found!", 404);
+    }
+
+    return response()->json([
+      "message" => "Successfully Get User data.",
+      "user" => $user,
+    ]);
   }
 
   /**
@@ -47,7 +68,25 @@ class UserController extends Controller
    */
   public function update(Request $request, string $id)
   {
-    //
+    $validated = $request->validate([
+      "name" => "sometimes|required|string|max:255",
+      "email" => "sometimes|required|string|email|max:255|unique:users,email,{$id}",
+      "password" => "sometimes|required|string|min:8|confirmed",
+      "nim" => "sometimes|required|unique:users,nim,{$id}|string|max:255",
+      "role" => "sometimes|string|max:255",
+    ]);
+
+    $user = User::find($id);
+    if (!$user) {
+      throw new UsersException("User with id {$id} not found!", 404);
+    }
+
+    $user->update($validated);
+
+    return response()->json([
+      "message" => "Successfully Update User data.",
+      "user" => $user,
+    ]);
   }
 
   /**
@@ -55,6 +94,17 @@ class UserController extends Controller
    */
   public function destroy(string $id)
   {
-    //
+    $user = User::find($id);
+
+    if (!$user) {
+      throw new UsersException("User with id {$id} not found!", 404);
+    }
+
+    $user->delete();
+
+    return response()->json([
+      "message" => "Successfully Delete User data.",
+      "user" => $user,
+    ]);
   }
 }
